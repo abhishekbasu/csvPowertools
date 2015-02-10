2f935342084a795ead502d1860b70c4e1114656c
Abhishek Basu <abhishek.sfy@gmail.com>
2015-02-10 18:07:43 +0530

    Some CSV handlers

---
+++ file_separator.py
@@ -0,0 +1,25 @@
+# File separator
+# Purpose is to generate csv files for each busid
+
+import csv
+
+def DATAINPUT(filename):
+    inputFile = open(filename,"r")
+    dictmain = {}
+    i=0
+    for line in inputFile:
+        d=str.strip(line)
+        k=d.split(",")
+        reqlength= int(k[1])
+        dictmain[str(k[0])+" "+str(k[1])+" "+str(k[2])]=(str(k[3]))
+    inputFile.close()
+    return dictmain
+
+def DATAOUTPUT(dictionaryfile):
+	for keys in dictionaryfile:
+		writer = csv.writer(open(("TF"+str(keys).split(" ")[0]+".csv"), 'a'))
+		writer.writerow([keys.split(" ")[1],keys.split(" ")[2],dictionaryfile[keys]])
+
+dictmain=DATAINPUT("tripstops_day1.csv")
+print dictmain
+DATAOUTPUT(dictmain)
